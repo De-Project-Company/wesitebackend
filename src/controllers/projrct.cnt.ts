b085a2e44cb7da9a7e53fd6ddd36c29765d75023
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-06-10 19:04:33 +0100

    Update projrct.cnt.ts

--- src/controllers/projrct.cnt.ts
+++ src/controllers/projrct.cnt.ts
@@ -21,7 +21,7 @@ const getProjectbyId = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "Id is needed" });
   }
   try {
-    const user = await prisma.project.findUnique({
+    const project = await prisma.project.findUnique({
       where: { id },
       include: {
         members: true,
@@ -31,7 +31,7 @@ const getProjectbyId = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    return res.status(200).json({ member: user });
+    return res.status(200).json({ project: project });
   } catch (error) {
     console.error("Error fetching users:", error);
     return res.status(500).json({ message: "Internal server error" });
